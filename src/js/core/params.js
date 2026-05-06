0f15412ee16e982c3f3b7a8a1f2e2e6a548dca05
Maggie Nichols <mn17@williams.edu>
2026-05-06 00:07:38 -0400

    Created new stimuli for window version. Updated image parameters and relevant file paths.

--- src/js/core/params.js
+++ src/js/core/params.js
@@ -4,7 +4,7 @@ Defining Parameter Variables
 ===============================================================
 */
 
-var stimFolder = 'src/assets/stimuli/monitor/'
+var stimFolder = 'src/assets/stimuli/window/'
 
 var runIntro = true;
 var runInstr = true;
@@ -49,9 +49,9 @@ var h =
     document.body.clientHeight;
 
 // setting display image width
-var origWidth = 371;
-var origHeight = 228;
-var imgWidth = origWidth; // your desired display img width
-var imgHeight = (imgWidth / origWidth) * origHeight;
+var origWidth = 560;
+var origHeight = 370;
+var imgWidth = origWidth
+var imgHeight = ((imgWidth / origWidth) * origHeight)
 
 
